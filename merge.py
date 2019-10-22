215e238ddf4907ab51a6d59a30524c5e0e4e6735
Juan Mosquera <maxiplux@gmail.com>
2019-10-21 21:58:03 -0400

    Firts commit

---
+++ merge.py
@@ -0,0 +1,22 @@
+def merge(array):
+    if len(array)>1:
+        len_array=len(array)
+        left_array=array[:len_array//2]
+        right_array=array[len_array//2:]
+        
+        merge(left_array)
+        merge(right_array)
+
+        array_index=0
+        left_index=0
+        right_index=0
+
+        while left_index < len(left_array) and right_index < len(right_array):
+            if left_array[left_index] < right_array[right_index]:
+                array[array_index]=left_array[left_index]
+                left_index=left_index+1
+            else:
+                array[array_index]=right_array[right_index]
+            array_index=array_index+1
+            
+        while array_index < len(left_array) :
